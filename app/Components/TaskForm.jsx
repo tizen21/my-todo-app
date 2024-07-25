4c3100a4a3374af5d9fa2bfb1d8ee8a1cba32e4b
Antho <rey.anthony1@gmail.com>
2024-07-25 13:59:00 +0200

    responsive lg & style

--- app/Components/TaskForm.jsx
+++ app/Components/TaskForm.jsx
@@ -85,15 +85,15 @@ export default function TaskForm() {
     <>
       <ul
         role="list"
-        className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
+        className="m-4 divide-y divide-gray-100 overflow-hidden bg-white shadow-md ring-1 ring-gray-900/5 sm:rounded-xl "
       >
         <li className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
           <div className="flex min-w-0 gap-x-4 justify-center items-center"></div>
-          <div className="flex w-full justify-center items-center gap-x-4">
+          <div className="flex w-full justify-center gap-x-4 items-center">
             <input
               type="text"
               placeholder="Add a task ..."
-              className="input input-bordered input-md w-full max-w-xs focus:ring-2 ring- ring-offset-2 focus:placeholder:opacity-0"
+              className="input input-bordered input-md w-full max-w-xs shadow focus:ring-2 ring- ring-offset-2 focus:placeholder:opacity-0"
               value={task}
               onChange={handleChange}
               onKeyDown={handleKeyDown}
@@ -123,70 +123,72 @@ export default function TaskForm() {
       <div className="divider divider-info">
         {tasks.length > 0 ? <p className="text-dark">{tasks.length}</p> : null}
       </div>
-      {tasks.map((item) => {
-        return (
-          <li
-            key={item.id}
-            className="flex flex-col items-center p-6 bg-gray-50 sm:px-6 mb-2 rounded-2xl shadow-sm shadow-info transition-all duration-300"
-          >
-            <div className="flex items-center justify-between w-full">
-              {editTaskId === item.id ? (
-                <input
-                  type="text"
-                  value={editTaskText}
-                  onChange={handleEditChange}
-                  onKeyDown={(e) => handleEditKeyDown(e, item.id)}
-                  className="input input-bordered input-md w-full max-w-xs"
-                />
-              ) : (
-                <p
-                  className={`font-semibold text-md ${
-                    item.completed ? "line-through text-gray-400" : ""
-                  } transition-all duration-300`}
-                >
-                  {item.task}
-                </p>
-              )}
-              <div className="flex gap-4 items-center">
+      <div className="lg:grid lg:grid-cols-2 xl:grid-cols-3">
+        {tasks.map((item) => {
+          return (
+            <li
+              key={item.id}
+              className="flex flex-col items-center p-6 m-4 bg-gray-50 sm:px-6 mb-2 rounded-2xl shadow-md transition-all duration-300"
+            >
+              <div className="flex items-center justify-between w-full">
                 {editTaskId === item.id ? (
-                  <>
-                    <CheckIcon
-                      aria-hidden="true"
-                      className="h-6 w-6 text-green-400 hover:cursor-pointer hover:text-green-600"
-                      onClick={() => handleSaveEdit(item.id)}
-                    />
-                    <XMarkIcon
-                      aria-hidden="true"
-                      className="h-6 w-6 text-red-400 hover:cursor-pointer hover:text-red-600"
-                      onClick={handleCancelEdit}
-                    />
-                  </>
+                  <input
+                    type="text"
+                    value={editTaskText}
+                    onChange={handleEditChange}
+                    onKeyDown={(e) => handleEditKeyDown(e, item.id)}
+                    className="input input-bordered input-sm w-1/2 max-w-x"
+                  />
                 ) : (
-                  <>
-                    <PencilIcon
-                      aria-hidden="true"
-                      className="h-6 w-6 text-gray-400 hover:cursor-pointer hover:text-black"
-                      onClick={() => handleEditClick(item.id, item.task)}
-                    />
-                    <input
-                      type="checkbox"
-                      checked={item.completed}
-                      id={`task-${item.id}`}
-                      className="checkbox size-6 transition-all duration-300"
-                      onChange={() => handleCheck(item.id)}
-                    />
-                    <TrashIcon
-                      aria-hidden="true"
-                      className="h-6 w-6 text-gray-400 hover:cursor-pointer hover:text-black"
-                      onClick={() => deleteTask(item.id)}
-                    />
-                  </>
+                  <p
+                    className={`font-semibold text-md ${
+                      item.completed ? "line-through text-green-400" : ""
+                    } transition-all duration-300`}
+                  >
+                    {item.task}
+                  </p>
                 )}
+                <div className="flex gap-6 items-center">
+                  {editTaskId === item.id ? (
+                    <>
+                      <CheckIcon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-green-400 hover:cursor-pointer hover:text-green-600"
+                        onClick={() => handleSaveEdit(item.id)}
+                      />
+                      <XMarkIcon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-red-400 hover:cursor-pointer hover:text-red-600"
+                        onClick={handleCancelEdit}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <input
+                        type="checkbox"
+                        checked={item.completed}
+                        id={`task-${item.id}`}
+                        className="checkbox size-6 transition-all duration-300 hover:ring-2 ring-offset-2 ring-green-400"
+                        onChange={() => handleCheck(item.id)}
+                      />
+                      <PencilIcon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-gray-400 hover:cursor-pointer hover:text-info"
+                        onClick={() => handleEditClick(item.id, item.task)}
+                      />
+                      <TrashIcon
+                        aria-hidden="true"
+                        className="h-6 w-6 text-gray-400 hover:cursor-pointer hover:text-red-400"
+                        onClick={() => deleteTask(item.id)}
+                      />
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-          </li>
-        );
-      })}
+            </li>
+          );
+        })}
+      </div>
     </>
   );
 }
